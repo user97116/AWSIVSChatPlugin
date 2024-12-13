2c44ee7f058d4459376fea54639dc0d555dd4c7a
Amar Dahake <35018901+user97116@users.noreply.github.com>
2024-12-13 16:27:10 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "IVSBroadcaster",
+    name: "ivs_broadcaster",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "IVSBroadcaster",
-            targets: ["IVSBroadcaster"]
+            name: "ivs_broadcaster",
+            targets: ["ivs_broadcaster"]
         ),
     ],
     targets: [
@@ -19,12 +19,12 @@ let package = Package(
             checksum: "c92ac3adc061a3fa5558311f8d99fa9b9dba7a00b3482a4f4ac90b2ff4f65b66"
         ),
         .target(
-            name: "IVSBroadcaster",
+            name: "ivs_broadcaster",
             dependencies: ["AmazonIVSChatMessaging"] // Ensuring the target depends on the binary target
         ),
         .testTarget(
-            name: "IVSBroadcasterTests",
-            dependencies: ["IVSBroadcaster"]
+            name: "ivs_broadcasterTests",
+            dependencies: ["ivs_broadcaster"]
         ),
     ]
 )
