2b6205c307e07ff0b5400e3d38e77188ce782aa5
Amar Dahake <35018901+user97116@users.noreply.github.com>
2025-02-18 11:28:35 +0530

    inits

--- Package.swift
+++ Package.swift
@@ -1,19 +1,35 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
-    name: "AmazonIVSChatMessaging",
+    name: "AWSChat",
+    platforms: [.iOS(.v14)], // Minimum iOS version is 14
     products: [
+        // Expose AWSChat as a library to other packages or modules
         .library(
-            name: "AmazonIVSChatMessaging",
-            targets: ["AmazonIVSChatMessaging"]),
+            name: "AWSChat",
+            targets: ["AWSChat"]
+        ),
+    ],
+    dependencies: [
+        // Add any other dependencies here if needed
     ],
     targets: [
-		.binaryTarget(
-			name: "AmazonIVSChatMessaging",
-			path: "./AmazonIVSChatMessaging/AmazonIVSChatMessaging.xcframework"
-		)
+        // Binary target for AmazonIVSChatMessaging
+        .binaryTarget(
+            name: "AmazonIVSChatMessaging",
+            url: "https://ivschat.live-video.net/1.0.0/AmazonIVSChatMessaging.xcframework.zip",
+            checksum: "c92ac3adc061a3fa5558311f8d99fa9b9dba7a00b3482a4f4ac90b2ff4f65b66"
+        ),
+        // Source code target for AWSChat
+        .target(
+            name: "AWSChat",
+            dependencies: ["AmazonIVSChatMessaging"] // Declare the binary target as a dependency
+        ),
+        // Test target for AWSChat
+        .testTarget(
+            name: "AWSChatTests",
+            dependencies: ["AWSChat"]
+        ),
     ]
 )
